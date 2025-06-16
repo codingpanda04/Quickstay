544144de6080f3dd72543b3bbf64cdd263f24dc4
Codingpanda04 <150348627+codingpanda04@users.noreply.github.com>
2025-06-16 12:41:22 +0000

    homepage

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,6 +3,8 @@ import Hero from '../components/Hero'
 import FeaturedDestination from '../components/FeaturedDestination'
 import ExclusiveOffers from '../components/ExclusiveOffers'
 import Testimonial from '../components/Testimonial'
+import Newsletter from '../components/Newsletter'
+import Footer from '../components/Footer'
 
 const Home = () => {
   return (
@@ -11,6 +13,7 @@ const Home = () => {
         <FeaturedDestination/>
         <ExclusiveOffers/>
         <Testimonial />
+        <Newsletter />
     </div>
   )
 }
