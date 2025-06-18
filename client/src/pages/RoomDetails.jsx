fa4ad53a7e77c510e095e4f143f87639d7fd92a0
Codingpanda04 <150348627+codingpanda04@users.noreply.github.com>
2025-06-18 08:35:49 +0000

    dope

--- client/src/pages/RoomDetails.jsx
+++ client/src/pages/RoomDetails.jsx
@@ -1,15 +1,17 @@
 import React, {useState, useEffect} from 'react'
 import {useParams} from 'react-router-dom'
 import {roomsDummyData} from '../assets/assets'
+import StarRating from '../components/StarRating'
+import {assets} from '../assets/assets'
 
 const RoomDetails = ()=>{
 
-    const {id} = useParams()
+        const {id} = useParams()
         const [room, setRoom] = useState(null)
         const [mainImage, setMainImage] = useState(null)
 
         useEffect(() => {
-            const room = roomsDummyData.find(room => room._id === room)
+            const room = roomsDummyData.find(room => room._id === id)
             room && setRoom(room)
             room && setMainImage(room.images[0])
         },[])
@@ -17,9 +19,39 @@ const RoomDetails = ()=>{
     return room && (
         <div className='py-28 md:py-35 px-4 md:px-16 lg:px-24 xl:px-32'>
             {/*Room Details */}
+            <div className='flex flex-col md:flex-row items-start md:items-center gap-2'>
+                <h1 className='text-3xl md:text-4xl font-playfair'>{room.hotel.name} <span className='font-inter text-sm'>({room.roomType})</span></h1>
+                <p className='font-inter text-xs text-white bg-orange-500 rounded-full py-1.5 px-3'>20% OFF</p>
+            </div>
+
+            {/*Room Rating */}
+            <div className='flex items-center gap-1 mt-2'>
+                <StarRating />
+                <p className='ml-2'>200 + Reviews</p>
+            </div>
+
+            {/*Room Address*/}
+            <div className='flex items-center gap-1 text-gray-500 mt-2'>
+                <img src={assets.locationIcon} />
+                <span>{room.hotel.address}</span>
+            </div>
+
+            {/*Room Images*/}
+            <div className='flex flex-col lg:flex-row mt-6 gap-6'>
+                <div className='lg:w-1/2 w-full'>
+                    <img src={mainImage} alt="Main Image" className='w-full rounded-xl shadow-lg object-cover'/>
+                </div>
+                <div className='grid grid-cols-2 gap-4 lg:w-1/2 w-full'>
+                    {room?.images.length > 1 && room.images.map((image, index)=>(
+                        <img onClick={()=> setMainImage(image)} src={image} key={index} alt="Main Image" 
+                        className={`w-full rounded-xl shadow-md object-cover cursor-pointer ${mainImage === image && 'outline-3 outline-orange-500'}`}/>
+                    ))}
+                </div>
+            </div>
+
+            {/*Room Highlights*/}
             <div>
-                <h1>{room.hotel.name} <span>({room.roomType})</span></h1>
-                <p>20% OFF</p>
+                
             </div>
         </div>
     )
