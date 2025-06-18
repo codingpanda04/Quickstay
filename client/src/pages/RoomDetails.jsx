93060a4d88d8ba111cf048179bc71fcf6995df45
Codingpanda04 <150348627+codingpanda04@users.noreply.github.com>
2025-06-18 06:58:19 +0000

    rooms

--- client/src/pages/RoomDetails.jsx
+++ client/src/pages/RoomDetails.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import {useParams} from 'react-router-dom'
+import {roomsDummyData} from '../assets/assets'
 
 const RoomDetails = ()=>{
-    return(
-        <div>
 
+    const {id} = useParams()
+        const [room, setRoom] = useState(null)
+        const [mainImage, setMainImage] = useState(null)
+
+        useEffect(() => {
+            const room = roomsDummyData.find(room => room._id === room)
+            room && setRoom(room)
+            room && setMainImage(room.images[0])
+        },[])
+
+    return room && (
+        <div className='py-28 md:py-35 px-4 md:px-16 lg:px-24 xl:px-32'>
+            {/*Room Details */}
+            <div>
+                <h1>{room.hotel.name} <span>({room.roomType})</span></h1>
+                <p>20% OFF</p>
+            </div>
         </div>
     )
 }
