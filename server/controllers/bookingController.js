4e6833c7a1f1cc828a3b14ab325fd2a3ec7e211e
Codingpanda04 <150348627+codingpanda04@users.noreply.github.com>
2025-06-30 11:33:13 +0000

    new

--- server/controllers/bookingController.js
+++ server/controllers/bookingController.js
@@ -7,8 +7,8 @@ const checkAvailability = async ({checkInDate, checkOutDate, room}) => {
     try {
         const bookings = await Booking.find({
             room,
-            checkInDate: {$lte: new checkOutDate},
-            checkOutDate: {$gte: new checkInDate}
+            checkInDate: {$lte: new Date(checkOutDate)},
+            checkOutDate: {$gte: new Date(checkInDate)}
         });
         const isAvailable = bookings.length === 0;
         return isAvailable;
